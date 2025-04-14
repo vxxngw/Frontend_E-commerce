4c42990181043dc270b18f7d40989002f192294d
nguyen thanh thien <nguyenthanhthien0609@gmail.com>
2025-04-14 14:42:37 +0700

    update

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
-  faShoppingCart,
+  faStore,
   faHouse,
   faBaby,
   faPeopleGroup,
@@ -17,6 +17,13 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { Carousel } from "react-bootstrap";
 import banner_main from "../Assets/banner5.png";
 import banner_main_4 from "../Assets/banner4.png";
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+// hoặc: import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 const Navbar = () => {
   const { all_product, getTotalCartAmount } = useContext(ShopContext);
@@ -83,38 +90,38 @@ const Navbar = () => {
                     <Link to="/shop">
                       <img
                         alt="Logo"
-                        src="https://pos.nvncdn.com/69e708-173581/store/20240425_lHnv9RXr.png"
+                        src="https://sdmntprsouthcentralus.oaiusercontent.com/files/00000000-6ad4-61f7-af8a-19e1bc4bb041/raw?se=2025-04-14T08%3A36%3A03Z&sp=r&sv=2024-08-04&sr=b&scid=47203214-a751-55bc-87e8-2cd8392aae47&skoid=ae70be19-8043-4428-a990-27c58b478304&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-04-14T06%3A57%3A26Z&ske=2025-04-15T06%3A57%3A26Z&sks=b&skv=2024-08-04&sig=I4MUtUYJipIYIZfaEV%2Bn8Y4bwNo0NPJXDiUQrTGQ9Vo%3D"
                       ></img>
                     </Link>
                   </div>
                 </div>
                 {/*Search*/}
-
+                
                 <div className="d-flex justify-content-center">
-                  <div
-                    className="col-md-9 col-sm-12 no-padding searchTop"
-                    id="searchtop"
-                  >
-                    <div className="search-collection col-xs-12 no-padding" onSubmit={handleSearchSubmit}>
-                      <form
-                        className="search align-items-center"
-                        action="/search"
-                      >
-                        <div className="custom-input-group">
-                          <input
-                            type="text"
-                            value={searchQuery}
-                            onChange={handleSearchChange}
-                            id="text-product"
-                            placeholder="Tìm kiếm sản phẩm bạn muốn mua"
-                          />
-                          <button id="search-button" className="btn btn-primary">
-                            <FontAwesomeIcon icon={faMagnifyingGlass} id="icon-search" />
-                          </button>
-                        </div>
-                      </form>
+                <div
+                  className="col-md-9 col-sm-12 no-padding searchTop"
+                  id="searchtop"
+                >
+                  <div className="search-collection col-xs-12 no-padding" onSubmit={handleSearchSubmit}>
+                    <form
+                      className="search align-items-center"
+                      action="/search"
+                    >
+                      <div className="custom-input-group">
+                      <input
+                        type="text"
+                        value={searchQuery}
+                        onChange={handleSearchChange}
+                        id="text-product"
+                        placeholder="Tìm kiếm sản phẩm bạn muốn mua"
+                      />
+                      <button id="search-button" className="btn btn-primary">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} id="icon-search" />
+                      </button>
                     </div>
+                    </form>
                   </div>
+                </div>
                 </div>
               </div>
               <div className="col-md-4 col-sm-12 d-flex">
@@ -124,12 +131,18 @@ const Navbar = () => {
                       <ul className="header-user d-none d-md-block">
                         <li>
                           <Link to="/login" className="but-login">
-                            <p>Dang nhap |</p>
+                            <p>
+                            <FontAwesomeIcon icon={faUser} style={{ marginRight: '0px' }} />
+                            Login
+                             </p>
                           </Link>
                         </li>
                         <li>
                           <Link to="/register" className="but-register">
-                            <p>Dang ky</p>
+                            <p>
+                            <FontAwesomeIcon icon={faUserPlus} style={{ marginRight: '0px' }} />
+                            Register
+                            </p>
                           </Link>
                         </li>
                       </ul>
@@ -142,7 +155,10 @@ const Navbar = () => {
                       <div id="cart_block" className="cart_relative">
                         <div className="cart-total-price">
                           <Link to="/cart" id="faShop">
-                            <FontAwesomeIcon icon={faShoppingCart} id="icon-cart" />
+                          <FontAwesomeIcon
+      icon={faShoppingCart} // 👈 đổi từ faStore sang faShoppingCart
+      id="icon-Shopping"
+    />
                           </Link>
                           <span
                             className={getTotalCartAmount() > 0 ? "" : "hidden"}
